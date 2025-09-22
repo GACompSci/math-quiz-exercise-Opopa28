2992f74efc0e4aa962160122ccc4cc37b9ba5c13
Om Popat <opopa28@germantownacademy.org>
2025-09-22 09:05:39 -0400

    hello

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+        Reader dog = new Reader();
+
+        String name = dog.getString("Enter Name:");
+        System.out.println(name);
+        int num_1 = Methods.randInt(1,10);
+        int num_2 = Methods.randInt(1,10);
+        int num_3 = Methods.randInt(1,10;
+        double num_4 = 6;
+
+
+
+
     }
 }
