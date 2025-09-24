f641c95e9ed08e00e1a9b7299f10e0504d0fb629
Om Popat <opopa28@germantownacademy.org>
2025-09-24 11:47:25 -0400

    hello

--- src/App.java
+++ src/App.java
@@ -23,45 +23,39 @@ public class App {
             System.out.println("Correct");
         } else {
             System.out.println("Wrong");
+            System.out.println("Correct awnser is: "+ c_awnser_1);
         }
+        //2
         int u_awnser2 = dog.getInt("What is "+num_2 + "-"+ num_3+"= ");
-                int c_awnser_2 = num_2 - num_3;
+        int c_awnser_2 = num_2 - num_3;
         if (u_awnser2 == c_awnser_2){
             score +=1;
             System.out.println("Correct");
         } else {
             System.out.println("Wrong");
+            System.out.println("Correct awnser is: "+ c_awnser_2);
         }
+        //3
         int u_awnser3 = dog.getInt("What is "+ num_3 + "*"+ rounded_num_4+"= ");
-                int c_awnser_3 = num_3 * rounded_num_4;
+        int c_awnser_3 = num_3 * rounded_num_4;
         if (u_awnser3 == c_awnser_3){
             score +=1;
             System.out.println("Correct");
         } else {
             System.out.println("Wrong");
+            System.out.println("Correct awnser is: "+ c_awnser_3);
         }
-
-
-
-        
+        // 4
         double u_awnser4 = Methods.roundDouble(0, dog.getDouble("What is "+ rounded_num_4 + "/"+ num_1+"= "));
         double c_awnser_4 =  (Methods.roundDouble(0, rounded_num_4 / num_1));
         if (u_awnser4 == c_awnser_4){
             score +=1;
             System.out.println("Correct");
         } else {
-            System.out.println(Methods.roundDouble(0, u_awnser4));
-            System.out.println(c_awnser_4);
             System.out.println("Wrong");
+            System.out.println("Correct awnser is: "+ c_awnser_4);
         }
-
-
-
-
-
-
-
-
+        // 5
         int u_awnser5 = dog.getInt("Find the remainder of: "+ num_2 + "/"+ rounded_num_4+ "= "); 
         int c_awnser_5 = (int) Methods.roundDouble(0, num_2 % rounded_num_4);
         if (u_awnser5 == c_awnser_5){
@@ -69,6 +63,7 @@ public class App {
             System.out.println("Correct");
         } else {
             System.out.println("Wrong");
+            System.out.println("Correct awnser is: "+ c_awnser_5);
         }
         System.out.println(score+"/5");
         // find the awnsers
